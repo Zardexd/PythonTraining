6c0d2e0a3dd96aea49c51146f6fd54a6182a78b2
Zardex <MailLais@proton.me>
2024-03-24 11:12:36 +0100

    How to name variables in Python

---
+++ Variables/VariableNames.py
@@ -0,0 +1,38 @@
+"""
+Variable names should be descriptive and make sense even to someone else first time reading code
+Valid symbols are (A-z, 0-9, and _), Variable names are case-sensitive.
+"""
+
+# Example of bad variable names
+
+a = 1
+b = 2
+c = 3
+
+foo = "abcd"
+
+# These may serve you as placeholder names while coding but should never make it to final solution,
+# unless they're used in something like a counter. Even there it might cause some confusion.
+
+""" Here is some examples how a good variable name might look like """
+
+# 1. `user_input`: Variable to store input provided by the user.
+# 2. `total_sales`: Variable to store the total sales amount.
+# 3. `average_temperature`: Variable to store the average temperature.
+# 4. `customer_name`: Variable to store the name of a customer.
+# 5. `employee_salary`: Variable to store the salary of an employee.
+# 6. `num_of_errors`: Variable to store the number of errors encountered.
+# 7. `max_value`: Variable to store the maximum value in a dataset.
+# 8. `min_temperature`: Variable to store the minimum temperature recorded.
+# 9. `is_valid_input`: Variable to store a boolean indicating whether input is valid or not.
+# 10. `file_contents`: Variable to store the contents read from a file.
+
+
+"""Variable name typing styles"""
+
+# Snake case
+user_input = "snake_case"
+# Camel case
+userInput = "camelCase"
+# Pascal case
+UserInput = 'PascalCase'
