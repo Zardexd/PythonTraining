09fcd381c2fb5dd5d43e17c156a757ddc2849d53
Zardex <MailLais@proton.me>
2024-03-24 11:13:09 +0100

    How to assign multiple values , or one value to many.

---
+++ Variables/MiltiValueAssignment.py
@@ -0,0 +1,18 @@
+# Many Values to Multiple Variables
+
+a, b, c = "Dog", "Cat", "Fish"
+print(a, b, c)
+
+# One Value to Multiple Variables
+x = y = z = "Banana"
+print(x)
+print(y)
+print(z)
+
+# Collection Unpacking
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
