4422d76e50b0364be79e618f12df74e8d72baf7a
Zardex <MailLais@proton.me>
2024-03-24 11:13:39 +0100

    Comments and how to write those

---
+++ Comments/Comments.py
@@ -0,0 +1,13 @@
+# This is a comment
+print('Hello Python')
+
+print('Hello Python Comments')  # Comments could be inserted like this too
+
+# Comments could be used to prevent code execution
+# def some_function():
+
+"""
+This could be used as a comment too, 
+since python ignores string literals that are not assigned to variables.
+This is often used to describe functions, some IDE's show these in the func preview.
+"""
